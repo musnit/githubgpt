5edaaf2e9c3a2792a4d502ca0aa9b4bf3ca51484
musnit <musnit@gmail.com>
2023-04-15 18:12:29 -0700

    accept all files

--- services/file.py
+++ services/file.py
@@ -2,11 +2,6 @@ import os
 from io import BufferedReader
 from typing import Optional
 from fastapi import UploadFile
-import mimetypes
-from PyPDF2 import PdfReader
-import docx2txt
-import csv
-import pptx
 
 from models.models import Document, DocumentMetadata
 
@@ -24,16 +19,6 @@ async def get_document_from_file(
 def extract_text_from_filepath(filepath: str, mimetype: Optional[str] = None) -> str:
     """Return the text content of a file given its filepath."""
 
-    if mimetype is None:
-        # Get the mimetype of the file based on its extension
-        mimetype, _ = mimetypes.guess_type(filepath)
-
-    if not mimetype:
-        if filepath.endswith(".md"):
-            mimetype = "text/markdown"
-        else:
-            raise Exception("Unsupported file type")
-
     try:
         with open(filepath, "rb") as file:
             extracted_text = extract_text_from_file(file, mimetype)
@@ -45,44 +30,7 @@ def extract_text_from_filepath(filepath: str, mimetype: Optional[str] = None) ->
 
 
 def extract_text_from_file(file: BufferedReader, mimetype: str) -> str:
-    if mimetype == "application/pdf":
-        # Extract text from pdf using PyPDF2
-        reader = PdfReader(file)
-        extracted_text = " ".join([page.extract_text() for page in reader.pages])
-    elif mimetype == "text/plain" or mimetype == "text/markdown":
-        # Read text from plain text file
-        extracted_text = file.read().decode("utf-8")
-    elif (
-        mimetype
-        == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    ):
-        # Extract text from docx using docx2txt
-        extracted_text = docx2txt.process(file)
-    elif mimetype == "text/csv":
-        # Extract text from csv using csv module
-        extracted_text = ""
-        decoded_buffer = (line.decode("utf-8") for line in file)
-        reader = csv.reader(decoded_buffer)
-        for row in reader:
-            extracted_text += " ".join(row) + "\n"
-    elif (
-        mimetype
-        == "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-    ):
-        # Extract text from pptx using python-pptx
-        extracted_text = ""
-        presentation = pptx.Presentation(file)
-        for slide in presentation.slides:
-            for shape in slide.shapes:
-                if shape.has_text_frame:
-                    for paragraph in shape.text_frame.paragraphs:
-                        for run in paragraph.runs:
-                            extracted_text += run.text + " "
-                    extracted_text += "\n"
-    else:
-        # Unsupported file type
-        raise ValueError("Unsupported file type: {}".format(mimetype))
-
+    extracted_text = file.read().decode("utf-8")
     return extracted_text
 
 
